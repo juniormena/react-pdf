3abe3875a2e7224c41de97b4dc3ca168bcc116ae
Junior Mena <jmenadiazl@outlook.com>
2021-05-28 11:14:07 -0400

    Adding files programaticly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import SinglePagePDFViewer from './components/pdf/single-page';
 import Banner from "./Banner.pdf";
+import { useState } from 'react';
 
 function App() {
+  const [pdfFile, setPdfLife] = useState('');
+  console.log(pdfFile);
+
   return (
     <div className="App">
         <div className="App-pdf">
-          <SinglePagePDFViewer pdf={Banner} /> 
+          <SinglePagePDFViewer pdf={pdfFile} /> 
         </div>
+        <input type="file" onChange={e=>setPdfLife(e.target.files[0] /*|| URL.createObjectURL(e.target.files[0]*/)}/>
     </div>
   );
 }
